ada842e0de2982eb14d03bc473e6950887f27937
Tom <t.kneller@mail.de>
2018-09-30 17:06:48 +0200

    PlayerController splitted into Player and CharacterController

---
+++ Dream/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    private Rigidbody2D rigid2d;
+
+    private int direction = 270;
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+
+    private float dashStartTimer = 0;
+
+    void Start() {
+        rigid2d = GetComponent<Rigidbody2D>();
+    }
+
+    /* SetDirection
+     * 
+     *  Sets current direction the character is facing
+     */
+    private void SetDirection(float moveInputHorizontal, float moveInputVertical) {
+        // Player facing down
+        if (moveInputVertical < 0) {
+            direction = 270;
+        }
+        // Player facing up
+        else if (moveInputVertical > 0) {
+            direction = 90;
+        }
+        // Player facing left
+        else if (moveInputHorizontal < 0) {
+            direction = 0;
+        }
+        // Player facing right
+        else if (moveInputHorizontal > 0) {
+            direction = 180;
+        }
+    }
+
+    /* GetDirection
+     * 
+     * Returns the direction the character is facing. 
+     */
+    public int GetDirection() {
+        return direction;
+    }
+
+    /* Move
+     * 
+     * Character directional movement.
+     */
+    public bool Move(float moveInputHorizontal, float moveInputVertical, float speed) {
+        if (moveInputHorizontal == 0 && moveInputVertical == 0) {
+            return false;
+        }
+
+        moveInput = new Vector2(moveInputHorizontal, moveInputVertical);
+        moveVelocity = moveInput.normalized * speed;
+
+        rigid2d.MovePosition(rigid2d.position + moveVelocity * Time.fixedDeltaTime);
+        SetDirection(moveInputHorizontal, moveInputVertical);
+
+        return true;
+    }
+
+    /* Dash
+     * 
+     * Character starts to dash if the dash button is held down longer than the time
+     * specified in dashStartDelay.
+     * 
+     * Character stops dashing when the dash button is no longer held down. 
+     */
+    public bool Dash(bool dashInput, float dashStartDelay, float dashSpeed) {
+        if (dashInput == true) {
+
+            if (dashStartTimer >= dashStartDelay) {
+
+                // Player facing down
+                if (direction == 270) {
+                    moveVelocity = Vector2.down * dashSpeed;
+                }
+                // Player facing up
+                else if (direction == 90) {
+                    moveVelocity = Vector2.up * dashSpeed;
+                }
+                // Player facing left
+                else if (direction == 0) {
+                    moveVelocity = Vector2.left * dashSpeed;
+                }
+                // Player facing right
+                else if (direction == 180) {
+                    moveVelocity = Vector2.right * dashSpeed;
+                }
+
+                rigid2d.MovePosition(rigid2d.position + moveVelocity * Time.fixedDeltaTime);
+            }
+            else {
+                dashStartTimer += Time.fixedDeltaTime;
+            }
+
+            return true;
+        }
+
+        dashStartTimer = 0;
+        return false;
+    }
+}
---
+++ Dream/Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+    private Animator animator;
+
+    // Movement
+    private float moveInputHorizontal = 0;
+    private float moveInputVertical = 0;
+    [Header("Movement")]
+    [SerializeField]
+    [Range(1f, 10f)]
+    private float moveSpeed = 5f;
+    private bool isMoving = false;
+
+    // Dash
+    private bool dashInput = false;
+    [SerializeField]
+    [Range(1f, 10f)]
+    private float dashSpeed = 10f;
+    [SerializeField]
+    [Range(0, 2f)]
+    private float dashStartDelay = 1f;
+    private bool isDashing = false;
+        
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate() {
+        isMoving = controller.Move(moveInputHorizontal, moveInputVertical, moveSpeed);
+        isDashing = controller.Dash(dashInput, dashStartDelay, dashSpeed);
+    }
+
+    void Update () {
+        moveInputHorizontal = Input.GetAxis("Horizontal");
+        moveInputVertical = Input.GetAxis("Vertical");
+        dashInput = Input.GetButton("Dash");
+
+        // Disable directional movement while dashing
+        if (dashInput == true) {
+            moveInputHorizontal = 0;
+            moveInputVertical = 0;
+        }
+
+        Animation();
+    }
+
+    /* DebugOutput
+     * 
+     * Prints debug information on screen.
+     */
+    void DebugOutput() {
+        GUI.Label(new Rect(50, 10, 300, 20), "Movement direction: " + controller.GetDirection());
+        GUI.Label(new Rect(50, 25, 300, 20), "Movement horizontal: " + moveInputHorizontal);
+        GUI.Label(new Rect(50, 40, 300, 20), "Movement vertical: " + moveInputVertical);
+        GUI.Label(new Rect(50, 55, 300, 20), "isMoving: " + isMoving);
+        GUI.Label(new Rect(50, 70, 300, 20), "Dash Input: " + dashInput);
+        GUI.Label(new Rect(50, 85, 300, 20), "isDashing: " + isDashing);
+    }
+
+    /* OnGui
+     * 
+     * Displays the GUI
+     */
+    void OnGUI() {
+        DebugOutput();
+    }
+
+    private void Animation() {
+        animator.SetInteger("Direction", controller.GetDirection());
+        animator.SetBool("isRunning", isMoving);
+    }
+}
